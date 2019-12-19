5b0f58ef7f85a528c20a8007b1422a7d9298e6bf
MambaBY <mambych@gmail.com>
2019-12-19 14:24:54 +0300

    Создана модель пользователя

---
+++ Fitness.BL/Model/Gender.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// Пол
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// название
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// создать новый пол
+        /// </summary>
+        /// <param name="name">Имя пола</param>
+        public Gender (string name)
+        {
+            //проверяем входные условия на предмет ошибок и некорректного ввода
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Пол не может быть пустым или NULL", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
